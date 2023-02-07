670b45632156fded2b86987e205805e6924e8379
Joaoof <joaodeus400@gmail.com>
2023-02-07 13:07:06 -0300

    commit 2

--- aprendendo.js
+++ aprendendo.js
@@ -1,3 +1,16 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("moongodb://localhost")
+// * Configurando o mongoose
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/aprendendo", {
+    useMongoClient: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("Mongo conectado...")
+  }).catch((err) => {
+    console.log("Deu erro aqui ó: " + err)
+  });
+
+  
